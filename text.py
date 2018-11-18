174acef955c5fd7e169e0bff17bd351a99594e7b
cmguyman11 <cmguyman11@gmail.com>
2018-11-18 10:10:17 -1000

    converting into csv files

--- text.py
+++ text.py
@@ -1,5 +1,10 @@
 import json
+import csv
+import sys
 from pprint import pprint
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 with open('./SpotifyDataset/file1.json') as f:
     rawData = json.load(f)
@@ -18,5 +23,29 @@ for i in range (0,len(allPlaylists)):
 	data.append(playlist)
 	results.append(allPlaylists[i]["num_followers"])
 
-print(data)
-print(results)
+# open a file for writing
+
+csv_data = open('./CSVData.csv', 'w+')
+
+# create the csv writer object
+
+csvwriter = csv.writer(csv_data)
+
+count = 0
+
+for d in allPlaylists:
+	del d["name"]
+	del d["pid"]
+	del d["modified_at"]
+	del d["tracks"]
+	if d.has_key("description"):
+		del d["description"]
+	if count == 0:
+		header = d.keys()
+
+		csvwriter.writerow(header)
+		count += 1
+	csvwriter.writerow(d.values())
+
+csv_data.close()
+
